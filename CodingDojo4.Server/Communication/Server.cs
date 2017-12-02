aa59f75947772fb5431cc901a0369dd719b460b8
Robert Chochole <wi16b065@technikum-wien.at>
2017-12-02 19:13:54 +0100

    Add project files.

---
+++ CodingDojo4.Server/Communication/Server.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo4.Server.Communication
+{
+    class Server
+    {
+
+        private TcpListener _Listener { get; set; }
+
+        private bool _stop = false;
+
+        public Server()
+        {
+            this._Listener = new TcpListener(IPAddress.Any, 1010);
+
+        }
+
+        public void Start()
+        {
+            this._Listener.Start();
+            while (!_stop)
+            {
+                if (this._Listener.Pending())
+                {
+                    var client = this._Listener.AcceptTcpClient();
+                    Console.WriteLine("New Client connected!");
+                }
+            }
+            this._Listener.Stop();
+        }
+
+        public void Stop()
+        {
+            this._stop = true;
+        }
+
+    }
+}
